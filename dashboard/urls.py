39da9ba80facf8745b34721405d44041bfd80a84
Udhaya-kumar24 <udhayaronaldo86@gmail.com>
2024-11-15 15:01:09 +0530

    API Issues fixed

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .api import *
+from .views import *
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'), 
     path('test', test, name='test')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 spark = SparkSession \
     .builder \
@@ -11,6 +12,11 @@ df = spark.read.option("header",True) \
     .option("inferSchema",True) \
     .csv("C:/React/Django/analytics-dashboard-assessment/dashboard/Electric_Vehicle_Population_Data.csv")
 
+def home(request):
+    return JsonResponse({'message': 'Welcome to the Dashboard API!'})
+
+@csrf_exempt
 def test(request):
     length = df.filter(df.County == 'King').count()
+    print('test', length,'::::::::::::::::::::::::::::::::::::::::::::::::::::')
     return JsonResponse({'res':length})
